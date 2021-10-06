87195a56c09cf53cef1d3528577123e7e4080bdd
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-10-06 16:08:32 +0100

    light and dark theme objects

---
+++ src/constants/Themes.js
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  body: "hsl(0, 0%, 98%)",
+  text: "hsl(235, 19%, 35%)",
+  disabled: "hsl(236, 9%, 61%)",
+  brightBlue: "hsl(220, 98%, 61%)",
+  listBg: "#fff",
+  status: "light",
+};
+
+export const darkTheme = {
+  body: "hsl(235, 21%, 11%)",
+  text: "hsl(234, 39%, 85%)",
+  disabled: "hsl(233, 14%, 35%)",
+  brightBlue: "hsl(220, 98%, 61%)",
+  listBg: "hsl(237, 14%, 26%)",
+  status: "dark",
+};
