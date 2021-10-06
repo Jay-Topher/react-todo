91aa9e7b3b022112f674f6ad3cd226b449b73221
Jones Ogolo <47540149+Jay-Topher@users.noreply.github.com>
2021-10-06 16:24:43 +0100

    create list item

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import styled from "styled-components";
+import closeIcon from "../images/icon-cross.svg";
+import checkIcon from "../images/icon-check.svg";
+
+function ListItem({ text, done, onDelete }) {
+  return (
+    <List className={`list-item ${done ? "done" : ""}`}>
+      <span className="radio" />
+      <p>{text}</p>
+      <button className="close-btn" onClick={onDelete} />
+    </List>
+  );
+}
+
+const List = styled.li`
+  padding: 1.5rem 2rem;
+  background-color: ${({ theme }) => theme.listBg};
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid ${({ theme }) => theme.disabled};
+
+  &.done {
+    p {
+      text-decoration: line-through;
+      color: ${({ theme }) => theme.disabled};
+    }
+
+    .radio {
+      background-image: url(${checkIcon}),
+        linear-gradient(to right, hsl(192, 100%, 67%), hsl(280, 87%, 65%));
+      background-position: center;
+      background-repeat: no-repeat;
+    }
+  }
+
+  &:hover {
+    .close-btn {
+      display: block;
+    }
+  }
+
+  .close-btn {
+    margin-left: auto;
+    border: none;
+    background-color: transparent;
+    background-image: url(${closeIcon});
+    width: 20px;
+    height: 20px;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: contain;
+    cursor: pointer;
+    transition: all 0.3s ease-in;
+    display: none;
+  }
+
+  p {
+    margin: 0;
+  }
+`;
+
+export default ListItem;
